b403102083d2ccfe2f19109f9158762542ec6fdd
Aidan Border <planes4u@outlook.com>
2020-04-18 13:50:15 -0400

    Attempting to add menu options

--- KextInstaller.sh
+++ KextInstaller.sh
@@ -5,15 +5,41 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 
 else
-    echo "fixing permissions on /System/Library/Extensions..."
-    command sudo chmod -R 755 /System/Library/Extensions
-    command sudo chown -R 0:0 /System/Library/Extensions
-    echo "Fixing permissions on /Library/Extensions... "
-    command sudo chmod -R 755 /Library/Extensions
-    command sudo chown -R 0:0 /Library/Extensions
-    echo "Done fixing permissions! Rebuilding kextcache.. "
-    command sudo kextcache -i /
-    echo "I'm all done! Have a nice rest of your day and good luck on all of your fruit shaped endeavors!"
-    echo "Script made by ABeeinSpace. Props to StackExchange for a lot of this script and sudo detection"
-    exit 0
+PS3='Please enter your choice: '
+options=("Fix Permissions for /S/L/E" "Fix Permissions for /L/E" "Rebuild Kextcache" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Fix Permissions for /S/L/E")
+            echo "fixing permissions on /System/Library/Extensions..."
+            command sudo chmod -R 755 /System/Library/Extensions
+            command sudo chown -R 0:0 /System/Library/Extensions
+            echo "Done fixing permissions!"
+            ;;
+        "Fix Permissions for /L/E")
+            echo "Fixing permissions on /Library/Extensions... "
+            command sudo chmod -R 755 /Library/Extensions
+            command sudo chown -R 0:0 /Library/Extensions
+            echo "Done fixing permissions!"
+            ;;
+        "Rebuild Kextcache")
+            command sudo kextcache -i /
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+
+
+
+
+#    echo "Done fixing permissions! Rebuilding kextcache.. "
+#    command sudo kextcache -i /
+#    echo "I'm all done! Have a nice rest of your day and good luck on all of your fruit shaped endeavors!"
+#    echo "Script made by ABeeinSpace. Props to StackExchange for a lot of this script and sudo detection"
+#    exit 0
 fi
